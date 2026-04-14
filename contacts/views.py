b1363243680d2995cc5e280a4c8e53726e600790
Kunaiev-Vladimir <140187@ukr.net>
2026-04-14 19:55:56 +0300

    обновил проект 14_04_2026

--- contacts/views.py
+++ contacts/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Contacts
 
 # Create your views here.
 
 def index(request):
     #1return HttpResponse("Contacts Index Page")
-    return render(request, 'contacts.html')
+    #2return render(request, 'contacts.html')
+    context = {
+        'contacts': Contacts.objects.first()
+    }
+    return render(request, 'contacts.html', context = context)
